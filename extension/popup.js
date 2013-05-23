ff217746339d5884a6e32912a3a1b009148f4511
Vadim Graboys <vadim@chartbeat.com>
2013-05-22 20:33:01 -0400

    domain works

--- extension/popup.js
+++ extension/popup.js
@@ -1,18 +1,25 @@
+var tabid;
+
+chrome.tabs.getSelected(null, function(tab) {
+    chrome.tabs.sendMessage(tab.id, {mtype: 'getFormInfo'}, function(response) {
+        tabid = tab.id;
+        console.log('got response', response);
+
+        var domain = response.domain;
+        console.log('setting domain', domain);
+        $('#domain').val(domain);
+    });
+});
+
 function submit(evt) {
     evt.preventDefault();
 
-    chrome.tabs.getSelected(null, function(tab) {
-        chrome.tabs.sendMessage(tab.id, {mtype: 'getFormInfo'}, function(response) {
-            console.log('got response', response);
+    var domain = $('#domain').val();
+    var password = domain + 'pass';
 
-            var domain = response.domain;
-            var password = domain + 'pass';
-
-            chrome.tabs.sendMessage(tab.id, {mtype: 'insertPasswords', password: password}, function() {
-                console.log('received reply');
-                window.close();
-            });
-        });
+    chrome.tabs.sendMessage(tabid, {mtype: 'insertPasswords', password: password}, function() {
+        console.log('received reply');
+        window.close();
     });
 }
 
